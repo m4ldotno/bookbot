eeb1eaa4865fc2151d384657f49be61f65bf3116
m4l <s4e@m4l.no>
2025-10-25 23:35:56 +0200

    Added char counter

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from stats import get_book_text, get_num_words
+from stats import get_book_text, get_num_words, get_num_chars
 
 def main():
     frankenstein = get_book_text("books/frankenstein.txt")
-    print(f"Found {get_num_words(frankenstein)} total words")
+    frankenstein_words = get_num_words(frankenstein)
+    frankenstein_chars = get_num_chars(frankenstein)
+
+    print(f"Found {frankenstein_words} total words")
+    print(frankenstein_chars)
 
 main()
--- stats.py
+++ stats.py
@@ -1,12 +1,21 @@
-def get_book_text(path_to_file):
+def get_book_text(path_to_file: str) -> str:
     book_strings = ""
     with open(path_to_file) as f:
         book_strings = f.read()
     return book_strings
 
-def get_num_words(book_text):
+def get_num_words(book_text: str) -> int:
     num_words = 0
     for word in book_text.split():
         num_words += 1
     return num_words
 
+def get_num_chars(book_text: str) -> int:
+    num_characters = {}
+    for word in book_text.split():
+        for char in word:
+            char = char.lower()
+            if char not in num_characters:
+                num_characters[char] = 0
+            num_characters[char] += 1
+    return num_characters
