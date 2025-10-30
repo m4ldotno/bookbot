cc4d31573a351c77f93708ca92dd76bba4cb7194
m4l <s4e@m4l.no>
2025-10-30 23:16:18 +0100

    added argument based book path

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from stats import get_book_text, get_num_words, get_num_chars, sort_chars
 
 def main():
+    import sys
+
+    if len(sys.argv) != 2:
+        print(f"Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
     print("=========== BOOKBOT ============")
-    book_path="books/frankenstein.txt"
+    book_path=sys.argv[1]
     book = get_book_text(book_path)
     print(f"Analyzing book found at {book_path}...")
 
